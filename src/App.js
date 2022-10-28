09d591e27a8e8ae7064164103cdd12c0e5df104c
Midokhalil1 <midokhalil22@gmail.com>
2022-10-28 10:42:34 -0400

    Hero

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Hero from "./components/Hero";
+
+import "./App.css";
 
 function App() {
+  const myName = "Mido";
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Hero />
+        {myName ? <p>Welcome Back.</p> : <button>Login</button>}
+        <p>Hello {myName || "guest"}.</p>
       </header>
     </div>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function Hero() {
+    const []
+
+    return (
+
+        <h1> This is React!</h1>
+    )
+
+}
+
+
+export default Hero 
